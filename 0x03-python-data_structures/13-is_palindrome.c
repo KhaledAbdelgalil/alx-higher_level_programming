072a4b860d4b52892a104e793998ac4d4f9afb3d
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2024-01-07 14:44:26 -0800

    task 13 in 0x03

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+
+listint_t* reverse_list(listint_t * list)
+{
+	listint_t* prev = NULL, *current = list, *nextNode = NULL;
+	while (current)
+	{
+		nextNode = current->next;
+		current->next = prev;
+		prev = current;
+		current = nextNode;
+	}
+	return (prev);
+}
+int compare_lists(listint_t* list1, listint_t* list2) {
+    while (list1 != NULL && list2 != NULL) {
+        if (list1->n != list2->n) {
+            return 0; // Not a palindrome
+        }
+        list1 = list1->next;
+        list2 = list2->next;
+    }
+
+    return 1; // Palindrome
+}
+int is_palindrome(listint_t **head)
+{
+	listint_t* slow = *head, *fast = *head, *prev_slow = *head;
+	listint_t *midNode = NULL, *secondHalf = NULL;
+	int palindrome = 1;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (0);
+	while (fast != NULL && fast->next != NULL)
+	{
+		prev_slow = slow;
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	if (fast != NULL)
+	{
+		midNode = slow;
+		slow = slow->next;
+	}
+	secondHalf = reverse_list(slow);
+	palindrome = compare_lists(*head, secondHalf);
+	secondHalf = reverse_list(secondHalf);
+	if (midNode != NULL) {
+        prev_slow->next = midNode;
+        midNode->next = secondHalf;
+    } else {
+        prev_slow->next = secondHalf;
+    }
+
+    return palindrome;
+}
+   
