d5e09dd280e94beb3829c095e56ecc393a95d38b
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2024-01-10 15:32:16 -0800

    task16 in 0x04

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,47 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+#include <bytesobject.h>
+
+void print_python_bytes(PyObject *p)
+{
+	long int size;
+	int i;
+	char *str = NULL;
+
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+	PyBytes_AsStringAndSize(p, &str, &size);
+	printf("  size: %li\n", size);
+	printf("  trying string: %s\n", str);
+	if (size < 10)
+		printf("  first %li bytes:", size + 1);
+	else
+		printf("  first 10 bytes:");
+	for (i = 0; i <= size && i < 10; i++)
+		printf(" %02hhx", str[i]);
+	printf("\n");
+}
+
+void print_python_list(PyObject *p)
+{
+        long int size = PyList_Size(p);
+        int i;
+        PyListObject *list = (PyListObject *)p;
+        char *type, *compared_str = "bytes";
+
+        printf("[*] Python list info\n");
+        printf("[*] Size of the Python List = %li\n", size);
+        printf("[*] Allocated = %li\n", list->allocated);
+        for (i = 0; i < size; i++)
+        {
+                type = (list->ob_item[i])->ob_type->tp_name;
+		printf("Element %i: %s\n", i, type);
+                if (strcmp(type, compared_str) == 0)
+                        print_python_bytes(list->ob_item[i]);
+        }
+}
