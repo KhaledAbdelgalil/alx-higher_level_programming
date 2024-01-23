bab56fb660b226c71a184b3e0d524e193fae462d
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2024-01-23 07:46:25 -0800

    task 6 in 0x06

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
 
-
-"""Defines a class Square."""
+"""Define a class Square."""
 
 
 class Square:
-    """Represents a square."""
+    """Represent a square."""
 
     def __init__(self, size=0, position=(0, 0)):
-        '''
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-        '''
         self.size = size
         self.position = position
 
-    def area(self):
-        return self.__size ** 2
-
     @property
     def size(self):
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
@@ -33,8 +22,23 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return (self.__size ** 2)
+
     def my_print(self):
-        """Print the square with the # character."""
         if self.__size == 0:
             print("")
             return
@@ -44,46 +48,3 @@ class Square:
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
-
-    @property
-    def position(self):
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        if not self.validate_position(value):
-            raise TypeError("Position must be a tuple of 2 positive integers")
-        self.__position = value
-
-    def validate_position(self, position):
-        cond = isinstance(position, tuple)
-        '''print(condition)'''
-        if not cond:
-            return False
-        cond = len(position) == 2
-        '''print(condition)'''
-        if not cond:
-            return False
-        cond = all(isinstance(coord, int) and coord >= 0 for coord in position)
-        '''print(condition)'''
-        if not cond:
-            return False
-        return True
-
-
-'''
-my_square_1 = Square(3)
-my_square_1.my_print()
-
-print("--")
-
-my_square_2 = Square(3, (1, 1))
-my_square_2.my_print()
-
-print("--")
-
-my_square_3 = Square(3, (3, 0))
-my_square_3.my_print()
-
-print("--")
-'''
