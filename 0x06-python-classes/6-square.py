6e8bc8cd860508d281010f3dc8bd8dbf0f8695a0
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2024-01-23 08:36:50 -0800

    task 8 in 0x06

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,7 +48,3 @@ class Square:
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
-
-
-    def __str__(self):
-        my_print(self)
