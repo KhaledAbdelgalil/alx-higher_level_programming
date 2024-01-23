796a4414a3b13433abad14b207a557c4739a6733
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2024-01-23 07:35:18 -0800

    task 6 in 0x06

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,14 +34,15 @@ class Square:
         self.__size = value
 
     def my_print(self):
+        if self.__size == 0:
+            print("")
+            return
         for i in range(self.__position[1]):
             print("")
 
         for i in range(self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for j in range(self.__size)]
-            print("")
-        if self.__size == 0:
             print("")
 
     @property
