bbbf634435d32aebb0dca8a67fe4c12eefd24116
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2024-01-23 07:42:50 -0800

    task 6 in 0x06

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,15 +34,15 @@ class Square:
         self.__size = value
 
     def my_print(self):
+        """Print the square with the # character."""
         if self.__size == 0:
             print("")
             return
-        for i in range(self.__position[1]):
-            print("")
 
-        for i in range(self.__size):
-            [print(" ", end="") for j in range(self.__position[0])]
-            [print("#", end="") for k in range(self.__size)]
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
             print("")
 
     @property
