2fb90eadcdfa4b4427c275922248114361fe4231
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2024-01-30 04:59:04 -0800

    task30 in 0x09

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,14 @@
+#!/usr/bin/python3
+"""Defines a locked class."""
+
+
 class LockedClass:
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+
+
     def __setattr__(self, name, value):
         if name != 'first_name':
             raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
