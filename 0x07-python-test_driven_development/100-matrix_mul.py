2a7cea2cc02564d5e0955e271046ab3c0f29ad22
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2024-02-02 10:16:11 -0800

    task 6 adding more testing 0x07

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -13,6 +13,11 @@ def matrix_mul(m_a, m_b):
         A new matrix representing the multiplication of m_a by m_b.
     """
 
+    if m_a == [] or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+    if m_b == [] or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
+
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
@@ -22,11 +27,6 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list of lists")
     if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
-
-    if len(m_a) == 0 or len(m_a[0]) == 0:
-        raise ValueError("m_a can't be empty")
-    if len(m_b) == 0 or len(m_b[0]) == 0:
-        raise ValueError("m_b can't be empty")
 
     if not all((isinstance(ele, int) or isinstance(ele, float))
                for ele in [num for row in m_a for num in row]):
