148ac647ea0e278abc910a93ce9c2a51123b348d
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2024-02-02 10:04:12 -0800

    task 1 solving betty style

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,16 +14,16 @@ def matrix_divided(matrix, div):
     Returns:
         A new matrix representing the result of the division.
     """
-    condition = (not isinstance(matrix, list) or matrix == []
-                or not all(isinstance(row, list) for row in matrix))
+    error = "matrix must be a matrix (list of lists) of integers/floats"
+    condition = (not isinstance(matrix, list))
+    condition = condition or (matrix == [])
+    condition = condition or (not all(isinstance(row, list) for row in matrix))
     if condition:
-        raise TypeError("matrix must be a matrix (list of lists) of "
-            "integers/floats")
-    condition =  not all((isinstance(ele, int) or isinstance(ele, float))
-                    for ele in [num for row in matrix for num in row])
+        raise TypeError(error)
+    condition = not all((isinstance(ele, int) or isinstance(ele, float))
+                        for ele in [num for row in matrix for num in row])
     if condition:
-        raise TypeError("matrix must be a matrix (list of lists) of "
-                        "integers/floats")
+        raise TypeError(error)
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
