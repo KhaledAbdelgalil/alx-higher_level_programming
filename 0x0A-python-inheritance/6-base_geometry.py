2251e1a2b7b28586932814eeef45f83ae350a96d
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2024-02-05 15:00:08 -0800

    Answering Task Number 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Task: 6. Base Geometry
+Write a class BaseGeometry
+By Khaled Mansour
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Public instance method that raises an Exception"""
+        raise Exception("area() is not implemented")
