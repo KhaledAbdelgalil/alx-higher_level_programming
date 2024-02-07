972b288a3ea497421e504b798a939e08c6a3a378
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2024-02-06 17:58:53 -0800

    Task #11 DONE

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -8,8 +8,6 @@ By Khaled Mansour
 
 
 class Student:
-    """Defines a student."""
-
     def __init__(self, first_name, last_name, age):
         """Instantiation with first_name, last_name, and age."""
         self.first_name = first_name
@@ -22,10 +20,22 @@ class Student:
         If attrs is a list of strings, retrieves only attributes in the list.
         Otherwise, retrieves all attributes.
         """
-        if (isinstance(attrs, list) and
-                all(isinstance(str_attr, str) for str_attr in attrs)):
-            return {
-                attr: getattr(self, attr)
-                for attr in attrs if hasattr(self, attr)
-            }
-        return self.__dict__
+        if isinstance(
+            attrs, list) and all(isinstance(
+                item, str) for item in attrs):
+            return {key: getattr(self, key)
+                    for key in attrs if hasattr(self, key)}
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance with values from json.
+        Assumes json will always be a dictionary with keys as attribute names.
+        """
+        for key, value in json.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
