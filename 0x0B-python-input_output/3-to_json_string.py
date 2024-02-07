59457f9cf8f13c61cc1972d36ea38fcf48521583
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2024-02-06 16:12:21 -0800

    Task #3 DONE

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -8,6 +8,6 @@ By Khaled Mansour
 
 
 def to_json_string(my_obj):
-	"""Returns the JSON representation of an object"""
-	import json
-	return json.dumps(my_obj)
+    """Returns the JSON representation of an object"""
+    import json
+    return json.dumps(my_obj)
