939fcb9ba097a9539d9c9a81ee48d34cc0cb5676
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2024-02-06 17:11:41 -0800

    Task #7 DONE

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Task: 7. Add Item
+Write a function that adds
+an item to a list
+By Khaled Mansour
+"""
+
+
+def add_item(my_list=[], new_item=""):
+    """Adds an item to a list"""
+    my_list.append(new_item)
+    return my_list
