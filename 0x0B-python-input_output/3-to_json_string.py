2776f3147914578507611e5d1ac25a044b033cec
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2024-02-06 16:11:07 -0800

    Task #3 DONE

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Task: 3. To JSON String
+Write a function that returns
+the JSON representation of an object
+By Khaled Mansour
+"""
+
+
+def to_json_string(my_obj):
+	"""Returns the JSON representation of an object"""
+	import json
+	return json.dumps(my_obj)
