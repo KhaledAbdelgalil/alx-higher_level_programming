ce66f70fdefd0cd526b4e1f5b1fe14cca6ce57c6
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2024-02-12 14:56:07 -0800

    Almost a circle - Task

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,12 +7,12 @@ class Rectangle(Base):
     """Represent a rectangle."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ Construct a new Rectangle instance """
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        
 
     @property
     def width(self):
@@ -20,6 +20,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        """ Set the width of the rectangle """
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -28,10 +29,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """ Return the height of the rectangle """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ Set the height of the rectangle """
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -40,10 +43,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """ Return the x coordinate of the rectangle """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """ Set the x coordinate of the rectangle """
         if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -52,66 +57,68 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """ Return the y coordinate of the rectangle """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """ Set the y coordinate of the rectangle """
         if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
 
-    def area(self):
-        return self.width * self.height
+    # def area(self):
+    #     return self.width * self.height
 
-    def display(self):
-        if self.width == 0 or self.height == 0:
-            print("")
-            return
+    # def display(self):
+    #     if self.width == 0 or self.height == 0:
+    #         print("")
+    #         return
 
-        [print("") for y in range(self.y)]
-        for h in range(self.height):
-            [print(" ", end="") for x in range(self.x)]
-            [print("#", end="") for w in range(self.width)]
-            print("")
+    #     [print("") for y in range(self.y)]
+    #     for h in range(self.height):
+    #         [print(" ", end="") for x in range(self.x)]
+    #         [print("#", end="") for w in range(self.width)]
+    #         print("")
 
-    def __str__(self):
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                       self.x, self.y,
-                                                       self.width, self.height)
+    # def __str__(self):
+    #     return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+    #                                                    self.x, self.y,
+    #                                                    self.width, self.height)
 
-    def update(self, *args, **kwargs):
+    # def update(self, *args, **kwargs):
+    #     if args and len(args) != 0:
+    #         arg_idx = 0
+    #         for arg in args:
+    #             if arg_idx == 0:
+    #                 if arg is None:
+    #                     self.__init__(self.width, self.height, self.x, self.y)
+    #                 else:
+    #                     self.id = arg
+    #             elif arg_idx == 1:
+    #                 self.width = arg
+    #             elif arg_idx == 2:
+    #                 self.height = arg
+    #             elif arg_idx == 3:
+    #                 self.x = arg
+    #             elif arg_idx == 4:
+    #                 self.y = arg
+    #             arg_idx = arg_idx + 1
+    #     elif kwargs and len(kwargs) != 0:
+    #         for k, v in kwargs.items():
+    #             if k == "id":
+    #                 if v is None:
+    #                     self.__init__(self.width, self.height, self.x, self.y)
+    #                 else:
+    #                     self.id = v
+    #             elif k == "width":
+    #                 self.width = v
+    #             elif k == "height":
+    #                 self.height = v
+    #             elif k == "x":
+    #                 self.x = v
+    #             elif k == "y":
+    #                 self.y = v
 
-        if args and len(args) != 0:
-            arg_idx = 0
-            for arg in args:
-                if arg_idx == 0:
-                    if arg is None:
-                        self.__init__(self.width, self.height, self.x, self.y)
-                    else:
-                        self.id = arg
-                elif arg_idx == 1:
-                    self.width = arg
-                elif arg_idx == 2:
-                    self.height = arg
-                elif arg_idx == 3:
-                    self.x = arg
-                elif arg_idx == 4:
-                    self.y = arg
-                arg_idx = arg_idx + 1
-        elif kwargs and len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "id":
-                    if v is None:
-                        self.__init__(self.width, self.height, self.x, self.y)
-                    else:
-                        self.id = v
-                elif k == "width":
-                    self.width = v
-                elif k == "height":
-                    self.height = v
-                elif k == "x":
-                    self.x = v
-                elif k == "y":
-                    self.y = v
