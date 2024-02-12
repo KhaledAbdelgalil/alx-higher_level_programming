f2004d73dee3649897c58ae4ce02809a215693da
KhaledAbdelgalil <khaled.abdelgalil96@gmail.ccom>
2024-02-12 13:50:58 -0800

    Almost a circle - Task

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Defines a base model class."""
+
+class Base:
+    """Base model.
+
+    This Represents the "base" for all other classes in the project.
+
+    Private Class Attributes:
+        __nb_object (int): Number of instantiated Bases.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
