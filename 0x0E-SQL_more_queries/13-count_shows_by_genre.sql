d3ac816e8818ca75c839722f570901d653017d51
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-03-16 10:49:23 +0000

    try another query

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,8 @@
 --- ON tv_genres.id = tv_show_genres.genre_id
 --- GROUP BY genre
 --- ORDER BY number_of_shows DESC;
-SELECT tv_genres.name AS 'genre',, COUNT(*) AS 'number_of_shows' from tv_genres INNER join tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
+SELECT tv_genres.name AS 'genre',, COUNT(*) AS 'number_of_shows'
+from tv_genres INNER join tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
