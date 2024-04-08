2d35122c8bcf8db647da9851153405d31a049f1d
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-04-08 23:31:33 +0100

    task 8 in warm js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const size = Math.floor(Number(process.argv[2]));
+if (!size) {
+  console.log('Missing size');
+} else {
+  for (let r = 0; r < size; r++) {
+    console.log('X'.repeat(size));
+  }
+}
