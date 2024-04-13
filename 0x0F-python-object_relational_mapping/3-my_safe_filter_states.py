2c087571d69268c67115a9ea6de42db50a2cdc8e
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-04-13 03:54:37 +0100

    task 3 in ORM python

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+list all states in the given database started by N
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    # Open database connection
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY %s \
+                   ORDER BY id;", (sys.argv[4],))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
