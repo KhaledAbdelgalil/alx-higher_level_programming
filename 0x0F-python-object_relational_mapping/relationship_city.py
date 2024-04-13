25a2173a6d84a947a521f312612a8c5e547e053d
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-04-13 21:34:39 +0100

    task 15 in ORM python

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -5,11 +5,13 @@ Contains the class definition of a City
 from relationship_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 
+
 class City(Base):
     """
     Class that defines each city
     """
     __tablename__ = 'cities'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True,
+                autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -15,6 +15,7 @@ class State(Base):
     Class with id and name attributes of each state
     """
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True,
+                autoincrement=True)
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="states")
