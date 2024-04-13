865cf4c925d74567bfdcf2f32fdde643c48f356c
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-04-13 02:51:57 +0100

    task 0 in ORM python

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+list all states in the given database
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    # Open database connection
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id;")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
