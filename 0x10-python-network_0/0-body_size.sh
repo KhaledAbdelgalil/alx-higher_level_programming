2765808925355e628cb3917e10b7b67948fde499
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-04-26 17:57:46 +0100

    task 0 in networks0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the byte size of response body for a given URL.
+# Bash script that takes in a URL then sends a request, and displays the size of the body of the response
 curl -s "$1" | wc -c
