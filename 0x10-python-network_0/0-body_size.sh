e882e47554d1cd9716ee8e16398ef13f4aadd166
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-04-26 10:06:30 -0700

    network0:0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL then sends a request, and displays the size of the body of the response
+# Script that takes in a URL then sends a request, and displays the size of the body of the response
 curl -s "$1" | wc -c
