de5333bf4f919a5a943e731d3dcfe694aec8163c
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-04-26 16:27:27 -0700

    task 9 in networks0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -sL -X put -H "khaled" 0.0.0.0:5000/catch_me -d "user_id=98"
+curl -sL -X put -H "Origin: khaled" 0.0.0.0:5000/catch_me -d "user_id=98"
