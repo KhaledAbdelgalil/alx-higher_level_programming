71fc7741e006088a7ea1652dcd217e8608f69d52
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-04-27 13:58:48 +0100

    task 7 in networks1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A script that
+- a Python script that takes in a URL,
+    sends a request to the URL and
+    displays the body of the response (decoded in utf-8).
+"""
+if __name__ == '__main__':
+    import requests
+    import sys
+    url = sys.argv[1]
+    res = requests.get(url)
+    if res.status_code < 400:
+        print(res.text)
+    else:
+        print("Error code: ", res.status_code)
