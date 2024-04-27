b891b3e5787aaa7d4408eb0895b4f3d21933cc5f
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-04-27 14:48:05 +0100

    task 10 in networks0

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A script that:
+- list 10 recent commits for github repo
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2],
+                                                              sys.argv[1])
+    response = requests.get(url)
+    for i in range(10):
+        print(response.json()[i].get("sha"), end=": ")
+        print(response.json()[i].get("commit").get("author").get("name"))
