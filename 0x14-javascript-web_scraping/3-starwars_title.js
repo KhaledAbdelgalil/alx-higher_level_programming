0cc7ae20f37a07497038559f9c0390bcdc8df7af
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-05-21 21:21:15 +0100

    task 3 web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const url = 'https://swapi-api.alx-tools.com/api/films/';
-const get_url = url + process.argv[2];
+let get_url = url + process.argv[2];
 const request = require('request');
 request(get_url, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
